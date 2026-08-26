c6b504f74342b8c9cbcebadba59239e668fec73c
Shubham Kumar <shubham040711@gmail.com>
2026-08-26 13:44:33 +0530

    fix(api): implement global asyncHandler to prevent process crashes
    Wrap all async controller methods in asyncHandler to catch unhandled promise rejections (e.g. duplicate job conflicts) and pass them to the global error handler middleware, preventing the Express server from crashing.

--- packages/api/src/modules/resume/resume.controller.js
+++ packages/api/src/modules/resume/resume.controller.js
@@ -1,7 +1,8 @@
 import * as resumeService from "./resume.service.js";
 import { ApiResponse } from "../../utils/ApiResponse.js";
+import { asyncHandler } from "../../utils/asyncHandler.js";
 
-export const uploadResume = async (req, res) => {
+export const uploadResume = asyncHandler(async (req, res) => {
   const resume = await resumeService.uploadResume(
     req.user._id,
     req.file,
@@ -11,17 +12,17 @@ export const uploadResume = async (req, res) => {
   return res
     .status(201)
     .json(ApiResponse.created(resume, "Resume uploaded"));
-};
+});
 
-export const getResumes = async (req, res) => {
+export const getResumes = asyncHandler(async (req, res) => {
   const resumes = await resumeService.getResumes(req.user._id);
 
   return res
     .status(200)
     .json(ApiResponse.ok(resumes));
-};
+});
 
-export const getResumeById = async (req, res) => {
+export const getResumeById = asyncHandler(async (req, res) => {
   const resume = await resumeService.getResumeById(
     req.user._id,
     req.params.id
@@ -30,12 +31,12 @@ export const getResumeById = async (req, res) => {
   return res
     .status(200)
     .json(ApiResponse.ok(resume));
-};
+});
 
-export const deleteResume = async (req, res) => {
+export const deleteResume = asyncHandler(async (req, res) => {
   await resumeService.deleteResume(req.user._id, req.params.id);
 
   return res
     .status(200)
     .json(ApiResponse.ok(null, "Resume deleted"));
-};
+});
--- packages/api/src/modules/template/template.controller.js
+++ packages/api/src/modules/template/template.controller.js
@@ -1,23 +1,24 @@
 import * as templateService from "./template.service.js";
 import { ApiResponse } from "../../utils/ApiResponse.js";
+import { asyncHandler } from "../../utils/asyncHandler.js";
 
-export const createTemplate = async (req, res) => {
+export const createTemplate = asyncHandler(async (req, res) => {
   const template = await templateService.createTemplate(req.user._id, req.body);
 
   return res
     .status(201)
     .json(ApiResponse.created(template, "Template created"));
-};
+});
 
-export const getTemplates = async (req, res) => {
+export const getTemplates = asyncHandler(async (req, res) => {
   const templates = await templateService.getTemplates(req.user._id);
 
   return res
     .status(200)
     .json(ApiResponse.ok(templates));
-};
+});
 
-export const getTemplateById = async (req, res) => {
+export const getTemplateById = asyncHandler(async (req, res) => {
   const template = await templateService.getTemplateById(
     req.user._id,
     req.params.id
@@ -26,9 +27,9 @@ export const getTemplateById = async (req, res) => {
   return res
     .status(200)
     .json(ApiResponse.ok(template));
-};
+});
 
-export const updateTemplate = async (req, res) => {
+export const updateTemplate = asyncHandler(async (req, res) => {
   const template = await templateService.updateTemplate(
     req.user._id,
     req.params.id,
@@ -38,12 +39,12 @@ export const updateTemplate = async (req, res) => {
   return res
     .status(200)
     .json(ApiResponse.ok(template, "Template updated"));
-};
+});
 
-export const deleteTemplate = async (req, res) => {
+export const deleteTemplate = asyncHandler(async (req, res) => {
   await templateService.deleteTemplate(req.user._id, req.params.id);
 
   return res
     .status(200)
     .json(ApiResponse.ok(null, "Template deleted"));
-};
+});
