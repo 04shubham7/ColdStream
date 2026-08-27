022154d5b89a24d3ff50df1b7956a33d9c277c87
Shubham Kumar <shubham040711@gmail.com>
2026-08-28 01:43:48 +0530

    fix: remove non-existent History route and ignore missing build scripts

--- packages/web/src/App.jsx
+++ packages/web/src/App.jsx
@@ -5,7 +5,6 @@ import ProtectedRoute from "./components/ProtectedRoute";
 import Layout from "./components/layout/Layout";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
-import History from "./pages/History";
 import Settings from "./pages/Settings";
 import Dashboard from "./pages/Dashboard";
 import Templates from "./pages/Templates";
@@ -34,16 +33,7 @@ function App() {
       <Routes>
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
-        <Route
-          path="/history"
-          element={
-            <ProtectedRoute>
-              <Layout>
-                <History />
-              </Layout>
-            </ProtectedRoute>
-          }
-        />
+
         <Route
           path="/settings"
           element={
